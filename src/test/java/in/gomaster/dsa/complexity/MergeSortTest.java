cd318089a8235180e5356ced01a4cf4c51f0b6b9
channabankapur <channabankapur@gmail.com>
2019-11-04 13:10:28 +0530

    SelectionSort and Mergesort profile test for only 1k and 10k.

--- src/test/java/in/gomaster/dsa/complexity/MergeSortTest.java
+++ src/test/java/in/gomaster/dsa/complexity/MergeSortTest.java
@@ -17,7 +17,8 @@ class MergeSortTest {
 	
 	@Test
 	void profileSort() {
-		int[] sizes = {1000, 10000, 100000};
+		int[] sizes = {1000, 10000};
+		//int[] sizes = {1000, 10000, 100000, 1000000};
 		for (int size : sizes) {
 			int[] testArray = Utils.genIntArray(size);
 			long start = System.nanoTime();
