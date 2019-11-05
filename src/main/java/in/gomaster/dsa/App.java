4fa54e27d32d16f07764f6edfbf8969dee6f8058
Narasimha Datta <ndatta@ubuntu>
2019-11-05 09:55:18 +0530

    Hello world class for testing

---
+++ src/main/java/in/gomaster/dsa/App.java
@@ -0,0 +1,17 @@
+package in.gomaster.dsa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static boolean hello() {
+		System.out.println("Hello world");
+		return true;
+	}
+	
+    public static void main( String[] args ) {
+        hello();
+    }
+}
---
+++ src/test/java/in/gomaster/dsa/AppTest.java
@@ -0,0 +1,14 @@
+package in.gomaster.dsa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void testHello() {
+		assertTrue(App.hello());
+	}
+
+}
